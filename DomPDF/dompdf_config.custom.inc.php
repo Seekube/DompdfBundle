baa37d39e2575df8c39f5967cd4767fa727404ae
David Peltier <david.peltier@seekube.com>
2014-02-12 10:12:26 +0100

    Init DomPDF + enable float in DomPDF config

--- DomPDF/dompdf_config.custom.inc.php
+++ DomPDF/dompdf_config.custom.inc.php
@@ -1,16 +1,18 @@
-<?php
+<?php 
 //define("DOMPDF_TEMP_DIR", "/tmp");
 //define("DOMPDF_CHROOT", DOMPDF_DIR);
-//define("DOMPDF_UNICODE_ENABLED", false);
+//define("DOMPDF_FONT_DIR", DOMPDF_DIR."/lib/fonts/");
+//define("DOMPDF_FONT_CACHE", DOMPDF_DIR."/lib/fonts/");
+//define("DOMPDF_UNICODE_ENABLED", true);
 //define("DOMPDF_PDF_BACKEND", "PDFLib");
 //define("DOMPDF_DEFAULT_MEDIA_TYPE", "print");
-define("DOMPDF_DEFAULT_PAPER_SIZE", "a4");
-define("DOMPDF_DEFAULT_FONT", "helvetica");
-define("DOMPDF_DPI", 150);
+//define("DOMPDF_DEFAULT_PAPER_SIZE", "letter");
+//define("DOMPDF_DEFAULT_FONT", "serif");
+//define("DOMPDF_DPI", 72);
 //define("DOMPDF_ENABLE_PHP", true);
-define("DOMPDF_ENABLE_REMOTE", true);
+//define("DOMPDF_ENABLE_REMOTE", true);
 //define("DOMPDF_ENABLE_CSS_FLOAT", true);
-//define("DOMPDF_ENABLE_JAVASCRIPT", true);
+//define("DOMPDF_ENABLE_JAVASCRIPT", false);
 //define("DEBUGPNG", true);
 //define("DEBUGKEEPTEMP", true);
 //define("DEBUGCSS", true);
